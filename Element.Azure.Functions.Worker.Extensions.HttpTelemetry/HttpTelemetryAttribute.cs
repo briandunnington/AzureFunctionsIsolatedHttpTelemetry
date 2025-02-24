c51bfdcd8b72630dda5f9aef54dc6bab53791851
briandunnington <briandunnington@gmail.com>
2025-02-24 13:03:34 -0800

    update to .NET 8
    and publish nuget package v1.1.0

--- Element.Azure.Functions.Worker.Extensions.HttpTelemetry/HttpTelemetryAttribute.cs
+++ Element.Azure.Functions.Worker.Extensions.HttpTelemetry/HttpTelemetryAttribute.cs
@@ -1,6 +1,6 @@
 ﻿using Microsoft.Azure.Functions.Worker.Extensions.Abstractions;
 
-[assembly: ExtensionInformation("Element.Azure.WebJobs.Extensions.HttpTelemetry", "1.0.3")]
+[assembly: ExtensionInformation("Element.Azure.WebJobs.Extensions.HttpTelemetry", "1.1.0")]
 
 namespace Element.Azure.Functions.Worker.Extensions.HttpTelemetry
 {
